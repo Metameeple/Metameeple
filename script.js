df592536a4f64d12949645b3b0b1756b14f3d10e
Metameeple <Metameeple.Business@gmail.com>
2025-06-06 18:58:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-// Supabase initialisieren – GANZ OBEN
+
 const supabaseUrl = 'https://oywfzyfzpencghrpqfdk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95d2Z6eWZ6cGVuY2docnBxZmRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMTc4ODUsImV4cCI6MjA2NDc5Mzg4NX0.OdMh5TH47gDdFYkWYQELxruXvdjhyLuMRfRjFJ1tywM';
-const supabase = supabaseJs.createClient(supabaseUrl, supabaseKey);
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 // UI basierend auf Login-Status umschalten
 function toggleUI(session) {

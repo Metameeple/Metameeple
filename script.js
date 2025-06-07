b4c407601709db490d537894d9b2ad3873a9f653
Metameeple <Metameeple.Business@gmail.com>
2025-06-07 20:14:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://oywfzyfzpencghrpqfdk.supabase.co';
-const supabaseKey = 'DEIN_ÖFFENTLICHER_API_KEY_HIER';
+const supabaseUrl = 'https://oywfzyfzpencghrpqfdk.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95d2Z6eWZ6cGVuY2docnBxZmRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMTc4ODUsImV4cCI6MjA2NDc5Mzg4NX0.OdMh5TH47gDdFYkWYQELxruXvdjhyLuMRfRjFJ1tywM';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // UI umschalten
